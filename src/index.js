2e8c131903e3756ced7075b662a2ecc79181befa
Zair Bulos <buloszair@gmail.com>
2025-03-18 19:12:20 -0300

    feat: Add initial bot setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(TOKEN);
